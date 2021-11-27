bd42cb638c7ea4db0559b5e5d7d984c796068f83
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2021-11-27 14:48:27 +0530

    Update Ec2-for-vpc-blog.sh

--- Bash_scripts/Ec2-for-vpc-blog.sh
+++ Bash_scripts/Ec2-for-vpc-blog.sh
@@ -1,2 +1,20 @@
+#!/bin/bash -ex
+
+#script
+echo '#!/bin/bash -ex
+yum -y update
+yum -y install httpd php mysql php-mysql
+chkconfig httpd on
+service httpd start
+cd /var/www/html
+wget https://s3-us-west-2.amazonaws.com/us-west-2-aws-training/awsu-spl/spl-13/scripts/app.tgz
+tar xvfz app.tgz
+chown apache:root /var/www/html/rds.conf.php' | tee script.sh 
+
+echo -n 'ENTER KeyPair Name: ' ; read KeyPair
+echo -n 'ENTER SECURITY GROUP ID: ' ; read sg
+echo -n 'ENTER SUBNET ID: ' ; read subnet
+
+
 #create instance
-aws ec2 run-instances --image-id $ami --count 1 --instance-type t2.micro --key-name $KeyPair --security-group-ids $sg --subnet-id $subnet
+aws ec2 run-instances --image-id $ami --count 1 --instance-type t2.micro --key-name $KeyPair --security-group-ids $sg --subnet-id $subnet --user-data file://script.sh
